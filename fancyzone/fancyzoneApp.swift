c5e8c7cd6bdb02f6b14fa3df8ba80ffbe6468318
Trevor Starick <trevor.starick@gmail.com>
2021-11-21 10:14:59 -0500

    Initial Commit

---
+++ fancyzone/fancyzoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  fancyzoneApp.swift
+//  fancyzone
+//
+//  Created by Trevor Starick on 2021-11-21.
+//
+
+import SwiftUI
+
+@main
+struct fancyzoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
